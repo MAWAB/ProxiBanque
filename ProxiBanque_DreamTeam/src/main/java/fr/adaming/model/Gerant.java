5525dafdcd825c5b0c72707ed97e9b9ff8d0853e
inti0294 <inti0294@DESKTOP-5LL73NR.home>
2016-11-24 17:20:48 +0100

    recap

--- ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/Gerant.java
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/Gerant.java
@@ -1,26 +1,49 @@
 package fr.adaming.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="gerants")
-public class Gerant extends Conseiller{
+public class Gerant extends Conseiller implements Serializable {
+	
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	// Propriétés -------------------------------------------------------------------------------------------------
 	
 	private String numeroBureau;
 	
 	//transormer l'agregation entre le conseiller et le gerant
-	private List<Conseiller> listeConselliers=new ArrayList<Conseiller>();
+	@OneToMany(mappedBy="gerant", fetch=FetchType.EAGER)
+	private List<Conseiller> listeConseillers=new ArrayList<Conseiller>();
 	//transormer l'agregation entre l'agence et le gerant
+	@OneToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="idAgence", referencedColumnName="idAgence")
 	private Agence agence;
 
+	
+	
+	// Constructeurs ---------------------------------------------------------------------------------------------
+	
+	public Gerant() {
+		super();
+	}
+	
 	/**
 	 * @param nom
 	 * @param prenom
@@ -38,13 +61,14 @@ public class Gerant extends Conseiller{
 	 */
 	public Gerant(String nom, String prenom, Date dateDeNaissance, Adresse adresse, int idConseiller,
 			String nomDuService, String numeroImmatriculation, String motDePasse, Gerant gerant,
-			List<Client> listeClients, String numeroBureau, List<Conseiller> listeConselliers, Agence agence) {
+			List<Client> listeClients, String numeroBureau, List<Conseiller> listeConseillers, Agence agence) {
 		super(nom, prenom, dateDeNaissance, adresse, idConseiller, nomDuService, numeroImmatriculation, motDePasse,
 				gerant, listeClients);
 		this.numeroBureau = numeroBureau;
-		this.listeConselliers = listeConselliers;
+		this.listeConseillers = listeConseillers;
 		this.agence = agence;
 	}
+
 	/**
 	 * @param nom
 	 * @param prenom
@@ -76,17 +100,18 @@ public class Gerant extends Conseiller{
 	public void setNumeroBureau(String numeroBureau) {
 		this.numeroBureau = numeroBureau;
 	}
+
 	/**
-	 * @return the listeConselliers
+	 * @return the listeConseillers
 	 */
-	public List<Conseiller> getListeConselliers() {
-		return listeConselliers;
+	public List<Conseiller> getListeConseillers() {
+		return listeConseillers;
 	}
 	/**
-	 * @param listeConselliers the listeConselliers to set
+	 * @param listeConseillers the listeConseillers to set
 	 */
-	public void setListeConselliers(List<Conseiller> listeConselliers) {
-		this.listeConselliers = listeConselliers;
+	public void setListeConseillers(List<Conseiller> listeConseillers) {
+		this.listeConseillers = listeConseillers;
 	}
 	/**
 	 * @return the agence
@@ -105,9 +130,14 @@ public class Gerant extends Conseiller{
 	 */
 	@Override
 	public String toString() {
-		return "Gerant [idGerant=" + idGerant + ", numeroBureau=" + numeroBureau + ", listeConselliers="
-				+ listeConselliers + ", agence=" + agence + "]";
+		return "Gerant [numeroBureau=" + numeroBureau + ", listeConseillers=" + listeConseillers + ", agence=" + agence
+				+ ", nom=" + nom + ", prenom=" + prenom + ", dateDeNaissance=" + dateDeNaissance + ", adresse="
+				+ adresse + "]";
 	}
+	
+	
+
+	
 	
 	
 	
