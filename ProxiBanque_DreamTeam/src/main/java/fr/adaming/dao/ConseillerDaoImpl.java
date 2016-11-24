15496cbba71ccfb1da2b30db69439943ff5caf2e
adelinep <adeline.pelloin@gmail.com>
2016-11-24 14:40:46 +0100

    modif persistence

---
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/dao/ConseillerDaoImpl.java
@@ -0,0 +1,43 @@
+package fr.adaming.dao;
+
+import java.util.List;
+
+import fr.adaming.model.Conseiller;
+
+public class ConseillerDaoImpl implements IConseillerDao {
+
+	
+	
+	
+	
+	@Override
+	public Conseiller getConseillerByIdDao(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Conseiller> getAllConseillerDao() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Conseiller addConseillerDao(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteConseillerDao(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Conseiller updateConseillerDao(Conseiller conseiller) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
