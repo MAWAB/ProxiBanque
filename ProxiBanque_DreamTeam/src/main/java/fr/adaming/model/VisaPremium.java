fcef576643ea752380ec7f792dbc296a25436d97
bentabon <bjm.soule@gmail.com>
2016-11-24 17:03:48 +0100

    Commit cartes

--- ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/VisaPremium.java
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/VisaPremium.java
@@ -3,71 +3,147 @@ package fr.adaming.model;
 import java.io.Serializable;
 import java.util.Date;
 
-public class VisaPremium extends Carte implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-/**
+@Entity
+@Table(name = "visapremiums")
+public class VisaPremium extends Carte implements Serializable {
+
+	/**
+		 * 
+		 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idVisaPremium;
+
+	@Temporal(TemporalType.DATE)
+	protected Date dateExpiration;
+
+	@OneToOne
+	@JoinColumn(name = "compte_id", referencedColumnName = "id_compteC")
+	private CompteCourant compteCourant;
+
+	private String plafondRetrait;
+
+	/**
+	 * @param idCarte
+	 * @param numeroCarte
+	 * @param dateExpiration
+	 * @param codePin
+	 * @param compteCourant
+	 * @param active
+	 * @param plafondRetrait
+	 */
+	public VisaPremium(int idCarte, String numeroCarte, Date dateExpiration, String codePin,
+			CompteCourant compteCourant, Boolean active, String plafondRetrait) {
+		super(numeroCarte, codePin, active);
+		this.idVisaPremium = idCarte;
+		this.plafondRetrait = plafondRetrait;
+		this.dateExpiration = dateExpiration;
+		this.compteCourant = compteCourant;
+	}
+
+	/**
+	 * @param numeroCarte
+	 * @param dateExpiration
+	 * @param codePin
+	 * @param compteCourant
+	 * @param active
+	 * @param plafondRetrait
+	 */
+	public VisaPremium(String numeroCarte, Date dateExpiration, String codePin, CompteCourant compteCourant,
+			Boolean active, String plafondRetrait) {
+		super(numeroCarte, codePin, active);
+		this.plafondRetrait = plafondRetrait;
+		this.dateExpiration = dateExpiration;
+		this.compteCourant = compteCourant;
+	}
+
+	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-private String plafondRetrait;
+	public VisaPremium() {
+		super();
+	}
 
-/**
- * @param idCarte
- * @param numeroCarte
- * @param dateExpiration
- * @param codePin
- * @param compteCourant
- * @param active
- * @param plafondRetrait
- */
-public VisaPremium(int idCarte, String numeroCarte, Date dateExpiration, String codePin, CompteCourant compteCourant,
-		Boolean active, String plafondRetrait) {
-	super(idCarte, numeroCarte, dateExpiration, codePin, compteCourant, active);
-	this.plafondRetrait = plafondRetrait;
-}
+	/**
+	 * @return the idVisaPremium
+	 */
+	public int getIdVisaPremium() {
+		return idVisaPremium;
+	}
 
-/**
- * @param numeroCarte
- * @param dateExpiration
- * @param codePin
- * @param compteCourant
- * @param active
- * @param plafondRetrait
- */
-public VisaPremium(String numeroCarte, Date dateExpiration, String codePin, CompteCourant compteCourant, Boolean active,
-		String plafondRetrait) {
-	super(numeroCarte, dateExpiration, codePin, compteCourant, active);
-	this.plafondRetrait = plafondRetrait;
-}
+	/**
+	 * @param idVisaPremium
+	 *            the idVisaPremium to set
+	 */
+	public void setIdVisaPremium(int idVisaPremium) {
+		this.idVisaPremium = idVisaPremium;
+	}
 
-/**
- * 
- */
-public VisaPremium() {
-	super();
-}
+	/**
+	 * @return the dateExpiration
+	 */
+	public Date getDateExpiration() {
+		return dateExpiration;
+	}
 
-/**
- * @return the plafondRetrait
- */
-public String getPlafondRetrait() {
-	return plafondRetrait;
-}
+	/**
+	 * @param dateExpiration
+	 *            the dateExpiration to set
+	 */
+	public void setDateExpiration(Date dateExpiration) {
+		this.dateExpiration = dateExpiration;
+	}
 
-/**
- * @param plafondRetrait the plafondRetrait to set
- */
-public void setPlafondRetrait(String plafondRetrait) {
-	this.plafondRetrait = plafondRetrait;
-}
+	/**
+	 * @return the compteCourant
+	 */
+	public CompteCourant getCompteCourant() {
+		return compteCourant;
+	}
 
-/* (non-Javadoc)
- * @see java.lang.Object#toString()
- */
-@Override
-public String toString() {
-	return "VisaPremium [plafondRetrait=" + plafondRetrait + "]";
-}
+	/**
+	 * @param compteCourant
+	 *            the compteCourant to set
+	 */
+	public void setCompteCourant(CompteCourant compteCourant) {
+		this.compteCourant = compteCourant;
+	}
 
+	/**
+	 * @return the plafondRetrait
+	 */
+	public String getPlafondRetrait() {
+		return plafondRetrait;
+	}
+
+	/**
+	 * @param plafondRetrait
+	 *            the plafondRetrait to set
+	 */
+	public void setPlafondRetrait(String plafondRetrait) {
+		this.plafondRetrait = plafondRetrait;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "VisaPremium [plafondRetrait=" + plafondRetrait + "]";
+	}
 
 }
