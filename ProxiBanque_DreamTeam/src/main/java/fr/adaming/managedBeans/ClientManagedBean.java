e552f26a37154801fa017417473996240f025c40
bentabon <bjm.soule@gmail.com>
2016-11-25 17:34:40 +0100

    Affichage infos client

---
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/managedBeans/ClientManagedBean.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package fr.adaming.managedBeans;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import fr.adaming.model.Client;
+import fr.adaming.service.IClientService;
+
+/**
+ * @author inti0302
+ *
+ */
+@ManagedBean(name="clientMB")
+@SessionScoped
+public class ClientManagedBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@ManagedProperty(value="#{clientServiceImpl}")
+	IClientService clientService;
+	
+	private	Client client;
+	private String adresse;
+	
+	HttpSession session;
+
+	/**
+	 * 
+	 */
+	public ClientManagedBean() {
+		super();
+	}
+
+	/**
+	 * @return the client
+	 */
+	public Client getClient() {
+		return client;
+	}
+
+	/**
+	 * @param client the client to set
+	 */
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	/**
+	 * @param clientService the clientService to set
+	 */
+	public void setClientService(IClientService clientService) {
+		this.clientService = clientService;
+	}
+	
+	@PostConstruct
+	public void init(){
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		
+		this.client = (Client) session.getAttribute("client");
+		this.adresse = "Rue" + this.client.getAdresse().getRue() + ", " + this.client.getAdresse().getCodePostal() + " " + this.client.getAdresse().getVille(); 
+	}
+	
+
+}
