0c9b654447755a8bae2b12be979a316f0fa9d173
inti0226 <inti0226@192.168.1.26>
2016-11-27 19:04:23 +0100

    Willem: CompteServiceDao Obstacle

--- ProxiBanque_DreamTeam/src/test/java/fr/adaming/serviceTest/CompteServiceTest.java
+++ ProxiBanque_DreamTeam/src/test/java/fr/adaming/serviceTest/CompteServiceTest.java
@@ -33,61 +33,61 @@ public class CompteServiceTest {
 	@Autowired
 	private ICompteService<Compte> compteService;
 		
-//	public static void main(String[] args) {
-//		
-//		ApplicationContext cxt=new FileSystemXmlApplicationContext("C:\\Users\\inti0226\\git\\ProxiBanque1\\ProxiBanque_DreamTeam\\src\\main\\webapp\\WEB-INF\\application-context.xml");
-//		
-//		ICompteService<Compte> compteService=(ICompteService<Compte>) cxt.getBean("cmpServiceBean");
-//		
-//
-//		String numeroCompte="81589";
-//		double solde=15000;
-//		int idCompte=2;
-//		double decouvert=5000;
-//		@SuppressWarnings("deprecation")
-//		Date dateCreation=new Date(2017,12,24);
-//		
-/////////////////////////////////////////////////
-//		Date date = new Date(1988 + 1900, 11, 7);
-//		Date dateCons = new Date(1970 + 1900, 02, 11);
-//		Date dateGer = new Date(1970 + 1900, 11, 23);
-//		Date dateAg = new Date(2016 + 1900, 11, 22);
-//
-//		Adresse adresse = new Adresse("Sco", 44000, "Orvault");
-//		Adresse adresseCons = new Adresse(1, "Remy", 33000, "Bordeaux");
-//		Adresse adresseGer = new Adresse(2, "Fleurus", 47000, "Agen");
-//
-//		List<Client> listeCl = null;
-//		List<Conseiller> listeCons = new ArrayList<Conseiller>();
-//
-//		Agence agence = null;
-//		Gerant gerant = null;
-//		gerant = new Gerant("Gerant", "gerant", dateGer, adresseGer, 2, "direction", "111111", "ger", gerant, listeCl,
-//				"1", listeCons, agence);
-//		agence = new Agence(1, "Nantes", dateAg, gerant, "1");
-//
-//		gerant.setGerant(gerant);
-//		gerant.setAgence(agence);
-//
-//		Conseiller conseiller = new Conseiller("Toto", "toto", dateCons, adresseCons, 1, "compta", "123456", "toto",
-//				gerant, listeCl);
-//		listeCons.add(conseiller);
-//
-//		gerant.setListeConseillers(listeCons);
-//
-//		Client cl = new Client("Soule", "Toto", date, adresse, 123, "0553474147", conseiller);
-/////////////////////////////////////////////////
-//		
-//		CompteCourant compteCourant=null;
-//		
-//		VisaElectron visaElectron=new VisaElectron("n5191959carte", dateCreation, "c254Pin", compteCourant, true, 19515619);
-//		VisaPremium visaPremium=new VisaPremium("n126119carte", dateCreation, "c654Pin", compteCourant, false, 5191);
-//		
-//		compteCourant=new CompteCourant(numeroCompte, solde, idCompte, decouvert, dateCreation, cl, visaElectron, visaPremium);
-//		
-//		
-//		compteService.ajouterCompteService(compteCourant);
-//	}
-//	
+	public static void main(String[] args) {
+		
+		ApplicationContext cxt=new FileSystemXmlApplicationContext("C:\\Users\\inti0226\\git\\ProxiBanque1\\ProxiBanque_DreamTeam\\src\\main\\webapp\\WEB-INF\\application-context.xml");
+		
+		ICompteService<Compte> compteService=(ICompteService<Compte>) cxt.getBean("cmpServiceBean");
+		
+
+		String numeroCompte="81589";
+		double solde=15000;
+		int idCompte=12;
+		double decouvert=5000;
+		@SuppressWarnings("deprecation")
+		Date dateCreation=new Date(2017,12,24);
+		
+///////////////////////////////////////////////
+		Date date = new Date(1988 + 1900, 11, 7);
+		Date dateCons = new Date(1970 + 1900, 02, 11);
+		Date dateGer = new Date(1970 + 1900, 11, 23);
+		Date dateAg = new Date(2016 + 1900, 11, 22);
+
+		Adresse adresse = new Adresse("Sco", 44000, "Orvault");
+		Adresse adresseCons = new Adresse(1, "Remy", 33000, "Bordeaux");
+		Adresse adresseGer = new Adresse(2, "Fleurus", 47000, "Agen");
+
+		List<Client> listeCl = null;
+		List<Conseiller> listeCons = new ArrayList<Conseiller>();
+
+		Agence agence = null;
+		Gerant gerant = null;
+		gerant = new Gerant("Gerant", "gerant", dateGer, adresseGer, 2, "direction", "111111", "ger", gerant, listeCl,
+				"1", listeCons, agence);
+		agence = new Agence(1, "Nantes", dateAg, gerant, "1");
+
+		gerant.setGerant(gerant);
+		gerant.setAgence(agence);
+
+		Conseiller conseiller = new Conseiller("Toto", "toto", dateCons, adresseCons, 1, "compta", "123456", "toto",
+				gerant, listeCl);
+		listeCons.add(conseiller);
+
+		gerant.setListeConseillers(listeCons);
+
+		Client cl = new Client("Soule", "Toto", date, adresse, 123, "0553474147", conseiller);
+///////////////////////////////////////////////
+		
+		CompteCourant compteCourant=null;
+		
+		VisaElectron visaElectron=new VisaElectron("n5191959carte", dateCreation, "c254Pin", compteCourant, true, 19515619);
+		VisaPremium visaPremium=new VisaPremium("n126119carte", dateCreation, "c654Pin", compteCourant, false, 5191);
+		
+		compteCourant=new CompteCourant(numeroCompte, solde, idCompte, decouvert, dateCreation, cl, visaElectron, visaPremium);
+		
+		
+		compteService.ajouterCompteService(compteCourant);
+	}
+	
 	
 }
