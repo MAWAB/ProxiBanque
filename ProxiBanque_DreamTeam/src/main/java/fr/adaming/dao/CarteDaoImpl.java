beb782e05759a2204c369f1ff545ff9d6147cb08
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2016-11-28 14:54:27 +0100

    Arnaud : modification de CarteDaoImpl (méthode supprimer)

--- ProxiBanque_DreamTeam/src/main/java/fr/adaming/dao/CarteDaoImpl.java
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/dao/CarteDaoImpl.java
@@ -103,14 +103,26 @@ public class CarteDaoImpl <T extends Carte> implements ICarteDao<T> {
 	public void ajouterCarteDao(T carte) {
 		EntityManager em = emf.createEntityManager();
 		if(carte instanceof VisaElectron){
-			em.getTransaction().begin();
-			em.persist(carte);
-			em.getTransaction().commit();
+			try{
+				em.getTransaction().begin();
+				em.persist(carte);
+				em.getTransaction().commit();
+				
+			}catch (NullPointerException | IllegalArgumentException e){
+				e.printStackTrace();
+			}
 			
-		}else if (carte instanceof VisaElectron) {
-			em.getTransaction().begin();
-			em.persist(carte);
-			em.getTransaction().commit();
+			
+		}else if (carte instanceof VisaPremium) {
+			try{
+				em.getTransaction().begin();
+				em.persist(carte);
+				em.getTransaction().commit();
+				
+			} catch (NullPointerException | IllegalArgumentException e){
+				e.printStackTrace();
+			}
+			
 		}
 		
 	}
@@ -118,13 +130,95 @@ public class CarteDaoImpl <T extends Carte> implements ICarteDao<T> {
 	@Override
 	public void modifierCarteDao(T carte) {
 		EntityManager em = emf.createEntityManager();
-		
+		if(carte instanceof VisaElectron){
+			try{
+				em.getTransaction().begin();
+				String req = "SELECT vE FROM VisaElectron vE WHERE vE.idVisaElectron=:vE_id";
+				Query query = em.createQuery(req);
+				query.setParameter("vE_id", ((VisaElectron) carte).getIdVisaElectron());
+				
+				VisaElectron vE=(VisaElectron) query.getSingleResult();
+				
+				vE.setActive(carte.getActive());
+				vE.setCodePin(carte.getCodePin());
+				vE.setCompteCourant(((VisaElectron) carte).getCompteCourant());
+				vE.setDateExpiration(((VisaElectron) carte).getDateExpiration());
+				vE.setNumeroCarte(carte.getNumeroCarte());
+				vE.setPlafondCredit(((VisaElectron) carte).getPlafondCredit());
+				
+				em.merge(vE);
+				em.getTransaction().commit();
+				
+				
+			}catch(NullPointerException | IllegalArgumentException e){
+				e.printStackTrace();
+			}
+		}else if (carte instanceof VisaPremium) {
+			try{
+				em.getTransaction().begin();
+				String req = "SELECT vP FROM VisaElectron vP WHERE vP.idVisaPremium=:vP_id";
+				Query query = em.createQuery(req);
+				query.setParameter("vP_id", ((VisaPremium) carte).getIdVisaPremium());
+				
+				VisaPremium vP = (VisaPremium) query.getSingleResult();
+				
+				vP.setActive(carte.getActive());
+				vP.setCodePin(carte.getCodePin());
+				vP.setCompteCourant(((VisaPremium) carte).getCompteCourant());
+				vP.setDateExpiration(((VisaPremium) carte).getDateExpiration());
+				vP.setNumeroCarte(carte.getNumeroCarte());
+				vP.setPlafondRetrait(((VisaPremium) carte).getPlafondRetrait());
+				
+				em.merge(vP);
+				em.getTransaction().commit();
+				
+				
+			}catch(NullPointerException | IllegalArgumentException e){
+				e.printStackTrace();
+			
+		}
 		
 	}
+}
 
 	@Override
 	public void supprimerCarteDao(T carte) {
-		// TODO Auto-generated method stub
+		EntityManager em = emf.createEntityManager();
+		
+		if(carte instanceof VisaElectron){
+			try{
+				em.getTransaction().begin();
+				String req = "SELECT vE FROM VisaElectron vE WHERE vE.idVisaElectron=:vE_id";
+				Query query = em.createQuery(req);
+				query.setParameter("vE_id", ((VisaElectron) carte).getIdVisaElectron());
+				
+				VisaElectron vE = (VisaElectron) query.getSingleResult();
+				
+				vE.setActive(false);
+				
+				em.merge(vE);
+				em.getTransaction().commit();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}else if (carte instanceof VisaPremium) {
+			try{
+				em.getTransaction().begin();
+				String req = "SELECT vP FROM VisaPremium vP WHERE vP.idVisaPremium=:vP_id";
+				Query query = em.createQuery(req);
+				query.setParameter("vP_id", ((VisaPremium) carte).getIdVisaPremium());
+				
+				VisaPremium vP = (VisaPremium) query.getSingleResult();
+				
+				vP.setActive(false);
+				
+				em.merge(vP);
+				em.getTransaction().commit();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
 		
 	}
 
