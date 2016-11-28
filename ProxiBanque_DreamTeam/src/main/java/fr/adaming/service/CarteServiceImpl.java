c3ab9322c52ebf98fb3463f43294207d85e5b13c
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2016-11-28 14:23:49 +0100

    Arnaud: CarteService

---
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/service/CarteServiceImpl.java
@@ -0,0 +1,55 @@
+package fr.adaming.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.ICarteDao;
+import fr.adaming.model.Carte;
+
+@Service("carteService")
+@Transactional
+public class CarteServiceImpl <T extends Carte> implements ICarteService<T> {
+	
+	@Autowired
+	private ICarteDao<Carte> carteDao;
+	
+	
+	
+	/**
+	 * @param carteDao the carteDao to set
+	 */
+	public void setCarteDao(ICarteDao<Carte> carteDao) {
+		this.carteDao = carteDao;
+	}
+
+	@Override
+	public T getCarteByIdService(T carte) {
+		return (T) carteDao.getCarteByIdDao(carte);
+	}
+
+	@Override
+	public T getCarteByIdCompteService(int id, T carte) {
+		return (T) carteDao.getCarteByIdCompteDao(id, carte);
+	}
+
+	@Override
+	public void ajouterCarteService(T carte) {
+		carteDao.ajouterCarteDao(carte);
+		
+	}
+
+	@Override
+	public void modifierCarteService(T carte) {
+		carteDao.modifierCarteDao(carte);
+		
+	}
+
+	@Override
+	public void supprimerCarteService(T carte) {
+		supprimerCarteService(carte);
+		
+	}
+	
+
+}
