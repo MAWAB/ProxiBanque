5e6a21dc41d9b38657f8dfb8d9d5b4c36cc53c04
adelinep <adeline.pelloin@gmail.com>
2016-11-28 17:47:29 +0100

    depot/versement

--- ProxiBanque_DreamTeam/src/main/java/fr/adaming/managedBeans/ClientManagedBean.java
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/managedBeans/ClientManagedBean.java
@@ -3,17 +3,23 @@
  */
 package fr.adaming.managedBeans;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import fr.adaming.model.Client;
+import fr.adaming.model.Compte;
+import fr.adaming.model.CompteCourant;
 import fr.adaming.service.IClientService;
+import fr.adaming.service.ICompteService;
 
 /**
  * @author inti0302
@@ -30,9 +36,15 @@ public class ClientManagedBean implements Serializable {
 
 	@ManagedProperty(value = "#{clientServiceImpl}")
 	IClientService clientService;
+	
+	@ManagedProperty(value="#{cmpServiceBean}")
+	ICompteService<Compte> compteService;
 
 	private Client client;
 	private String adresse;
+	
+	private CompteCourant compteCourant;
+	private double somme;
 
 	HttpSession session;
 
@@ -66,6 +78,21 @@ public class ClientManagedBean implements Serializable {
 		this.clientService = clientService;
 	}
 
+	
+	/**
+	 * @return the compteService
+	 */
+	public ICompteService<Compte> getCompteService() {
+		return compteService;
+	}
+
+	/**
+	 * @param compteService the compteService to set
+	 */
+	public void setCompteService(ICompteService<Compte> compteService) {
+		this.compteService = compteService;
+	}
+
 	/**
 	 * @return the adresse
 	 */
@@ -79,6 +106,34 @@ public class ClientManagedBean implements Serializable {
 	 */
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
+	}
+
+	/**
+	 * @return the compteCourant
+	 */
+	public CompteCourant getCompteCourant() {
+		return compteCourant;
+	}
+
+	/**
+	 * @param compteCourant the compteCourant to set
+	 */
+	public void setCompteCourant(CompteCourant compteCourant) {
+		this.compteCourant = compteCourant;
+	}
+
+	/**
+	 * @return the somme
+	 */
+	public double getSomme() {
+		return somme;
+	}
+
+	/**
+	 * @param somme the somme to set
+	 */
+	public void setSomme(double somme) {
+		this.somme = somme;
 	}
 
 	@PostConstruct
@@ -99,6 +154,36 @@ public class ClientManagedBean implements Serializable {
 	public String navigationAjoutCompteEp() {
 		session.setAttribute("client", clientService.getClientByIdService(this.client.getIdClient()));
 		return "ajoutCE.xhtml";
+	}
+	
+	public void retirer() throws IOException {
+		
+		compteCourant = client.getCompteCourant();
+		compteService.retraitService(compteCourant, somme);
+		client.setCompteCourant(compteCourant);
+		clientService.updateClientService(client);
+		
+		this.client = clientService.getClientByIdService(client.getIdClient());
+		session.setAttribute("client", client);
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+
+	}
+	
+	public String deposer() throws IOException {
+		
+		compteCourant = client.getCompteCourant();
+		compteService.depotService(compteCourant, somme);
+		client.setCompteCourant(compteCourant);
+		clientService.updateClientService(client);
+		
+		this.client = clientService.getClientByIdService(client.getIdClient());
+		session.setAttribute("client", client);
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+		this.client = (Client) session.getAttribute("client");
+		
+		return "infosClient.xhtml";
 	}
 
 }
