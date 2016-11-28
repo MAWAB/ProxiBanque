3298027719a900a7fc5ba0c26cd5595d694e805c
adelinep <adeline.pelloin@gmail.com>
2016-11-28 14:53:20 +0100

    ajout inheritance, model carte

--- ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/Carte.java
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/model/Carte.java
@@ -2,6 +2,12 @@ package fr.adaming.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class Carte implements Serializable {
 
 	/**
