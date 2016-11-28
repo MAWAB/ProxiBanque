1a925235ce1d68a967d1cc1e1f478b2fcf070b29
inti0294 <inti0294@DESKTOP-5LL73NR.home>
2016-11-28 16:03:22 +0100

    ajout Managed bean + pages html pour auditeur

---
+++ ProxiBanque_DreamTeam/src/main/java/fr/adaming/managedBeans/AuditeurManagedBean.java
@@ -0,0 +1,10 @@
+package fr.adaming.managedBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "auditeurMB")
+@SessionScoped
+public class AuditeurManagedBean {
+
+}
